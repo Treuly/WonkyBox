513abf293d1a7fbb7037f4a730fc72bce5e335a5
lindblemma <lindblemma@ecs.vuw.ac.nz>
2022-02-15 11:04:31 +1300

    delete unneeded

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import GoogleRecipe from '../components/GoogleRecipe';
+
 import RecipeForm from '../components/RecipeForm';
 
 const RecipePage = () => {
     return (
         <div>
             <RecipeForm />
-            {/* <GoogleRecipe /> */}
         </div>
     )
 }
